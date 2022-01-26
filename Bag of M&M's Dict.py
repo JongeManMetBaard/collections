420ab82fba2471d0d60931eec7a302647af1d382
ali <ali.gout.12@hotmail.com>
2022-01-26 11:12:52 +0100

    Dictionary

---
+++ Bag of M&M's Dict.py
@@ -0,0 +1,16 @@
+import random
+
+color = ["oranje", "blauw", "groen", "bruin"]
+
+def contentOfBag(amountOfmnm):
+    bag = {}
+    for x in range(amountOfmnm):
+        randomColor = random.choice(color)
+        if randomColor in bag:
+            bag[randomColor] += 1
+        else:
+            bag[randomColor] = 1
+    return bag
+
+amountOfmnm = int(input("How much M&M'S do you want?"))
+print(contentOfBag(amountOfmnm))
