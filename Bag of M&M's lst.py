65ed4b39dfb65d912902a6c12bab768a1460ec5e
ali <ali.gout.12@hotmail.com>
2022-01-26 11:18:29 +0100

    list

--- Bag of M&M's lst.py
+++ Bag of M&M's lst.py
@@ -2,15 +2,11 @@ import random
 
 color = ["oranje", "blauw", "groen", "bruin"]
 
-def contentOfBag(amountOfmnm):
-    bag = {}
-    for x in range(amountOfmnm):
-        randomColor = random.choice(color)
-        if randomColor in bag:
-            bag[randomColor] += 1
-        else:
-            bag[randomColor] = 1
+def contentOfBag(amountOfMMs):
+    bag = []
+    for x in range(amountOfMMS):
+        bag.append(random.choice(color))
     return bag
 
-amountOfmnm = int(input("How much M&M'S do you want?"))
-print(contentOfBag(amountOfmnm))
+amountOfMMS = int(input("How much M&M'S do you want?"))
+print(contentOfBag(amountOfMMS))
